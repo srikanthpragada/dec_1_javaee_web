8fba3af12ab827770cb7dafccb1c9e61984c201c
srikanthpragada <srikanthpragada@gmail.com>
2016-12-13 20:34:12 +0530

    13-DEC

--- src/servlets/NowServlet.java
+++ src/servlets/NowServlet.java
@@ -4,20 +4,27 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/now")
+// @WebServlet("/now")
 public class NowServlet extends HttpServlet {
+	int count=0;
 	
+	@Override 
+	public void init(ServletConfig config) {
+		String company = config.getInitParameter("company");
+		System.out.println(company);
+	}
+	@Override 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	     response.setContentType("text/html");
 	     PrintWriter out = response.getWriter();
 	     out.println("<html><h1>" + new Date() + "</h1></html>");
-	     
+	     out.println("<h3>" + ++count + "</h3");
 	}
 
 }
