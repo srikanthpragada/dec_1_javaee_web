757bf61e31eab4b400fb9e6b05e79c847adca34c
srikanthpragada <srikanthpragada@gmail.com>
2016-12-30 20:31:57 +0530

    30-DEC

---
+++ src/endpoints/UpperEndPoint.java
@@ -0,0 +1,19 @@
+package endpoints;
+
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/upper")
+public class UpperEndPoint {
+  
+    @OnMessage
+    public void onMessage(Session session, String message) {
+        try {
+           // System.out.println("message received :" + message);
+            session.getBasicRemote().sendText(message.toUpperCase());
+        } catch (Exception ex) {
+        }
+    }
+}
