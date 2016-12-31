708edf3b413034db42067970c72280b4f87175aa
srikanthpragada <srikanthpragada@gmail.com>
2016-12-31 20:36:39 +0530

    31-DEC

---
+++ src/ws/HelloService.java
@@ -0,0 +1,14 @@
+package ws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService 
+public class HelloService {
+	
+	 @WebMethod 
+	  public String getMessage() {
+		  return "Hello!";
+	  }
+
+}
