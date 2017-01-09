6c05e40155b28120e2885d6cbc26075e602fd154
srikanthpragada <srikanthpragada@gmail.com>
2017-01-09 20:31:42 +0530

    9-JAN

--- src/jsf/EmployeesBean.java
+++ src/jsf/EmployeesBean.java
@@ -1,5 +1,8 @@
 package jsf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.sql.RowSet;
 import javax.sql.rowset.CachedRowSet;
@@ -9,10 +12,9 @@ import oracle.jdbc.rowset.OracleCachedRowSet;
 @ManagedBean
 public class EmployeesBean {
 
-	// employees property 
+	// employees property
 	public RowSet getEmployees() {
-		try
-		{
+		try {
 			CachedRowSet rs = new OracleCachedRowSet();
 			rs.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
 			rs.setUsername("hr");
@@ -25,5 +27,52 @@ public class EmployeesBean {
 			return null;
 		}
 	}
+
+	// names property
+	public List<String> getNames() {
+		try {
+			CachedRowSet rs = new OracleCachedRowSet();
+			ArrayList<String> names = new ArrayList<>();
+			rs.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+			rs.setUsername("hr");
+			rs.setPassword("hr");
+			rs.setCommand("select * from employees");
+			rs.execute();
+			while( rs.next())
+			{
+				names.add( rs.getString("first_name"));
+			}
+			return names;
+		} catch (Exception ex) {
+			System.out.println(ex);
+			return null;
+		}
+	}
+
+	
+	// names property
+		public List<Employee> getAllEmployees() {
+			try {
+				CachedRowSet rs = new OracleCachedRowSet();
+				ArrayList<Employee> emps = new ArrayList<>();
+				rs.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+				rs.setUsername("hr");
+				rs.setPassword("hr");
+				rs.setCommand("select * from employees");
+				rs.execute();
+				while( rs.next())
+				{
+					Employee e = new Employee();
+					e.setId( rs.getInt("employee_id"));
+					e.setSalary( rs.getInt("salary"));
+					e.setName ( rs.getString("first_name"));
+					emps.add(e);
+				}
+				return emps;
+			} catch (Exception ex) {
+				System.out.println(ex);
+				return null;
+			}
+		}
 
 }
