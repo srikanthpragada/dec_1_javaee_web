2f4f07bcb3d0523697e5e3a1f0610a71ba446155
srikanthpragada <srikanthpragada@gmail.com>
2017-01-10 20:30:18 +0530

    10-JAN

--- src/jsf/LoginBean.java
+++ src/jsf/LoginBean.java
@@ -1,11 +1,20 @@
 package jsf;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.ValueChangeEvent;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @ManagedBean
 public class LoginBean {
 
 	private String username, password, message;
+	
+	// valuechangelistener
+	public void usernameChanged(ValueChangeEvent evt) {
+		System.out.println("Username : "  + username);
+		System.out.println("Username : "  + evt.getNewValue());
+	}
 	
 	// Action Controller 
 	// Return value is page name 
@@ -19,14 +28,18 @@ public class LoginBean {
 		}
 	}
 
+	@Size(min=5, max=10, message = "Username must be 5 to 10 chars")
+	@Pattern(regexp="[0-9a-zA-Z]+", message = "Invalid username. It must be alphanumeric")
 	public String getUsername() {
 		return username;
 	}
 
 	public void setUsername(String username) {
+		System.out.println("setUsername()");
 		this.username = username;
 	}
 
+	@Size(min=5, max=10, message = "Password must be 5 to 10 chars")
 	public String getPassword() {
 		return password;
 	}
